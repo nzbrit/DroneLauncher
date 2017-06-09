539b3556aa4c32ee00312f236aa226c9a292f33d
Stephen Bunce <Stephen.Bunce@trustpower.co.nz>
2017-06-09 15:38:21 +1200

    Add project files.

---
+++ DroneLauncher/DroneLauncher/Models/LandingParameters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DroneLauncher.Common;
+
+namespace DroneLauncher
+{
+    public enum LandingResultType { Landed, Kaboom, }
+    public class LandingParameters
+    {
+        public LandingParameters()
+        {
+            this.Altitude = CoreConstants.StartingAltitude;
+            this.Velocity = CoreConstants.StartingVelocity;
+            this.Fuel = CoreConstants.StartingFuel;
+            this.Thrust = CoreConstants.StartingThrust;
+        }
+
+        public double Altitude;
+        public double Velocity;
+        public double Fuel;
+        public double Thrust;
+    }
+}
